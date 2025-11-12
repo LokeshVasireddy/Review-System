9c09e7bae2569c602203e9d88a06aba59ed8c4ce
LokeshVasireddy <vasireddylokesh3@gmail.com>
2025-11-12 21:55:56 +0530

    tp

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,14 @@
-const express = require('exoress');
-const app=express();
-
-module.exports=app;
+const mongoose =require("mongoose");
+const dotenv=require("dotenv");
+dotenv.config({path:"./config.env"});
+const app=require("./app");
+const DB=process.env.DATABASE.replace("<PASSWORD>",process.env.DATABASE_PASSWORD);
+mongoose.connect(DB).then(()=>{
+    console.log("DB connection successful");
+}).catch((err)=>{
+    console.log(err);
+})
+const port=process.env.PORT;
+app.listen(port,()=>{
+    console.log("app running on port "+port);
+})
