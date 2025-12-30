84e797a78a0d8431b21a24b14387600168df3112
LokeshVasireddy <vasireddylokesh3@gmail.com>
2025-12-30 21:31:00 +0530

    jk

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,22 @@
-const express = require('exoress');
+const express = require('express');
+const heLmet=  require('helmet');
+const cors=require('cors');
+const morgon=require('morgan');
+const rateLimit=require('express-rate-limit');
+const mongosnatize=require('express-mongo-sanitize');
 const app=express();
+app.use(heLmet());
+app.use(cors({
+    origin: ["http://localhost:3000"],
+    credentials: true,
+}));
+const limiter=rateLimit({
+    max:10000,
+    windowMs:60*60*1000,
+    message:"Too many requests from this IP, please try again in an hour!",
+    standardHeaders:true,
+    legacyHeaders:false,
+});
+app.use('/api',limiter);
 
 module.exports=app;
--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,18 @@ const mongoose =require("mongoose");
 const dotenv=require("dotenv");
 dotenv.config({path:"./config.env"});
 const app=require("./app");
-const DB=process.env.DATABASE.replace("<PASSWORD>",process.env.DATABASE_PASSWORD);
-mongoose.connect(DB).then(()=>{
-    console.log("DB connection successful");
-}).catch((err)=>{
-    console.log(err);
-})
+//const DB=process.env.DATABASE.replace("<PASSWORD>",process.env.DATABASE_PASSWORD);
+// const DB=process.env.DB_URL;
+// mongoose.connect(DB).then(()=>{
+//     console.log("DB connection successful");
+// }).catch((err)=>{
+//     console.log(err);
+// })
+
+mongoose.connect(process.env.DATABASE)
+.then(() => console.log("Connected to local MongoDB"))
+.catch(err => console.log(err));
+
 const port=process.env.PORT;
 app.listen(port,()=>{
     console.log("app running on port "+port);
