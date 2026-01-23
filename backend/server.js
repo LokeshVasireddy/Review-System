0b29ed5c910d76308cf07aeac10bcc963a893703
LokeshVasireddy <vasireddylokesh3@gmail.com>
2026-01-23 19:16:54 +0530

    ks

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ app.listen(port,()=>{
     console.log("app running on port "+port);
 });
 process.on('unhandledRejection',err=>{
-    console.log('UNHANDLED REJECTION!  Shutting down...');
+    console.log('UNHANDLED REJECTION!  Shutting down..');
     console.log(err.name, err.message);
     server.close(()=>{
         process.exit(1);
