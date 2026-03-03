154e6c8e64355dc6b5e3ffb7728badbd63a0f823
LokeshVasireddy <vasireddylokesh3@gmail.com>
2026-03-03 19:04:53 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,13 @@ const mongoose =require("mongoose");
 const dotenv=require("dotenv");
 dotenv.config({path:"./config.env"});
 const app=require("./app");
-//const DB=process.env.DATABASE.replace("<PASSWORD>",process.env.DATABASE_PASSWORD);
-// const DB=process.env.DB_URL;
-// mongoose.connect(DB).then(()=>{
-//     console.log("DB connection successful");
-// }).catch((err)=>{
-//     console.log(err);
-// })
+const DB=process.env.DATABASE.replace("<PASSWORD>",process.env.DATABASE_PASSWORD);
+const DB=process.env.DB_URL;
+mongoose.connect(DB).then(()=>{
+    console.log("DB connection successful");
+}).catch((err)=>{
+    console.log(err);
+})
 process.on('uncaughtException',err=>{
     console.log('UNCAUGHT EXCEPTION!  Shutting down...');
     console.log(err.name, err.message);
